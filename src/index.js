1d13ed1c23ccd5a6db989d2cf6618c765a72dc3c
heiets <o.heiets@gmail.com>
2017-09-10 16:23:21 +0300

    package fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import 'babel-polyfill'
-import React from 'react'
+import 'babel-polyfill';
+import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
